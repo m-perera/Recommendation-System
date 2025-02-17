438973a21b78b8094cce66d26234e9997a5f6370
m-perera <nationofthewhitelotus@gmail.com>
2025-02-16 23:02:52 -0500

    Add files via upload

---
+++ gradio-dash.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+import gradio as gr
+
+books = pd.read_csv("books_with_emotions.csv")
+books["large_thumbnail"] = books["thumbnail"] + "&fife=w800"
+books["large_thumbnail"] = np.where(
+    books["large_thumbnail"].isna(),
+    "cover-not-found.jpg",
+    books["large_thumbnail"]
+)
+
+raw_documents = TextLoader("tagged_des.txt").load()
+text_splitter = CharacterTextSplitter(chunk_size=0, chunk_overlap=0, separator="\n")
+documents = text_splitter.split_documents(raw_documents)
+db_books = Chroma.from_documents(
+    documents,
+    embedding= HuggingFaceEmbeddings()
+) 
+
+def give_semantic_recommendations(
+        query: str,
+        category: str = None,
+        initial_top_k: int = 50,
+        final_top_k: int = 16,
+) -> pd.DataFrame:
+    recs = db_books.similarity_search(query, k = initial_top_k)
+    books_list = [int(rec.page_content.strip('"').split()[0]) for rec in recs]
+    book_recs = books[books["isbn13"].isin(books_list)].head(final_top_k)
+
+    if category != "All":
+        book_recs = book_recs[book_recs["simple_cat"]== category][:final_top_k]
+    else:
+        books_recs = book_recs.head(final_top_k)
+    
+    return books_recs
+
+
+# Visuals of Dashbord 
+
+def recommend_books(query: str, category: str):
+    recommendations = give_semantic_recommendations(query,category)
+    results = []
+    for _, row in recommendations.iterrows():
+        description = row["description"]
+        truncated_desc_split = description.split()
+        truncated_description = " ".join(truncated_desc_split[:30]) + "..."
+
+        authors_split = row["authors"].split(";")
+        if len(authors_split) == 2:
+            authors_str = f"{authors_split[0]} and {authors_split[1]}"
+        elif len(authors_split) > 2:
+            authors_str = f"{', '.join(authors_split[:-1])}, and {authors_split[-1]}"
+        else:
+            authors_str = row["authors"]
+
+        caption = f"{row["title"]} by {authors_str}: {truncated_description}"
+        results.append((row["large_thumbnail"], caption))
+    return results
+
+
+categories = ["All"] + sorted(books["simple_cat"].dropna().astype(str).unique())
+
+with gr.Blocks(theme=gr.themes.Glass()) as dashbord:
+    gr.Markdown("# Semantic Book Recommender")
+
+    with gr.Row():
+        user_query = gr.Textbox(label = "Please enter a description of a book:", placeholder= "eg. A story about sports")
+        category_dropdown = gr.Dropdown(choices=categories, label="Select a category:", value="All")
+        submit_button = gr.Button(value="Find Recommendation")
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label="Recommended Books", columns=8, rows=2 )
+    submit_button.click(fn = recommend_books, inputs=[user_query, category_dropdown], outputs=output)
+
+if __name__ == "__main__":
+    dashbord.launch(share=True)
+    
+
+
